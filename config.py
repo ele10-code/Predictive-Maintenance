cbe2cc07222127918040c9005f47bcf668705a54
ele10-code <elena.zerbin.ez@gmail.com>
2024-07-24 10:23:15 +0200

    data analysis for Seritel

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+# Description: This file contains the configuration for the database connection.
+# Development Server
+DB_USER_SERVER1 = 'root'
+DB_PASSWORD_SERVER1 = 'txCtrlZZhbg@20!'
+DB_NAME_SERVER1 = 'txControl'
 
-# Configurazioni dal file config
-DB_USER = 'root'
-DB_PASSWORD = 'txCtrlZZhbg@20!'
-DB_NAME = 'txControl'
-
-
-
-
+# Seritel Server
+DB_USER_SERVER2 = 'root'
+DB_PASSWORD_SERVER2 = 'txCtrlZZhbg@20!'
+DB_NAME_SERVER2 = 'txControl'
 
